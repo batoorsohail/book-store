5793b4667765eab5fce829686d6e6c8b57cfa975
unknown <sohailshahidi1435@gmail.com>
2023-03-15 19:28:00 +0430

    Use Redux in React

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,28 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = ({ title, author }) => (
-  <div className="book-card">
-    <p>{title}</p>
-    <p>{author}</p>
-    <button type="button" className="remove-btn">Remove</button>
-  </div>
-);
+const Book = ({ title, author, id }) => {
+  const dispatch = useDispatch();
+
+  const onRemoveBook = (e) => {
+    const { id } = e.target.dataset;
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <div className="book-card">
+      <p>{title}</p>
+      <p>{author}</p>
+      <button type="button" data-id={id} className="remove-btn" onClick={onRemoveBook}>Remove</button>
+    </div>
+  );
+};
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 import '../styles/Form.css';
 
-const Form = () => (
-  <div className="form-container">
-    <form className="form">
-      <input type="text" placeholder="Title" required />
-      <input type="text" placeholder="Author" required />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!title.trim() || !author.trim()) return;
+    const bookData = {
+      item_id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(bookData));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div className="form-container">
+      <form className="form" onSubmit={submitHandler}>
+        <input type="text" placeholder="Title" onChange={onTitleChange} required />
+        <input type="text" placeholder="Author" onChange={onAuthorChange} required />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
