f383228b5ceb2319a94dae49127f2097e8bdec1d
unknown <sohailshahidi1435@gmail.com>
2023-03-17 11:08:50 +0430

    Add final style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import UserIcon from '../icons';
 import '../index.css';
 
 const Navbar = () => (
@@ -7,12 +8,15 @@ const Navbar = () => (
     <h1>Book Store</h1>
     <ul className="menu-container">
       <li>
-        <Link to="/">Books</Link>
+        <Link to="/" className="nav-links">Books</Link>
       </li>
       <li>
-        <Link to="/categories">Categories</Link>
+        <Link to="/categories" className="nav-links">Categories</Link>
       </li>
     </ul>
+    <div className="icon-list">
+      <UserIcon />
+    </div>
   </nav>
 );
 
